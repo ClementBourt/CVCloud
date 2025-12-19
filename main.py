20cc18aa601d6361466f1946c0c7cd755f579055
Bourtguize Clement <clement.bourtguize@hotmail.com>
2025-12-19 14:16:57 +0100

    added an arg to the index function as an attempt to trouble shout cloud deployment

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from markupsafe import escape
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def index(arg_for_cloud):
     cv_url=url_for('static',filename='cv_Bourtguize_Ramel_Clement.pdf')
     return f'<a href = {cv_url}>CV lin</a>'
